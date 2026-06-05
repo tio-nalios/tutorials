e545526bc5f0a13355a0ca55432c1388bb3dcb32
Tim Olde <tio@nalios.be>
2026-06-05 09:55:37 +0200

    finished tutorial chapter 4

--- estate/__manifest__.py
+++ estate/__manifest__.py
@@ -3,4 +3,9 @@
     'category': 'Sales/Real Estate',
     'installable': True,
     'application': True,
+    'author': 'Tim Olde',
+    'license': 'LGPL-3',
+    'data': [
+        'security/ir.model.access.csv',
+    ]
 }
