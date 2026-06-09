106c1fdb0394191e4d6b225cb83ed264df0919cb
Tim Olde <tio@nalios.be>
2026-06-09 10:11:32 +0200

    empty estate_account module

---
+++ estate_account/__manifest__.py
@@ -0,0 +1,13 @@
+{
+    'name': 'Real Estate Invoicing',
+    'category': 'Sales/Real Estate',
+    'installable': False,
+    'application': False,
+    'author': 'Tim Olde',
+    'license': 'LGPL-3',
+    'depends': ['estate', 'account'],
+    'auto_install': True,
+    'data': [
+        
+    ]
+}
